202c34173c0949583bda7d8113cfd3e87be6a9a0
Manos Daskalelis <Manolisdaskalelis@gmai.com>
2024-11-23 22:42:01 +0200

    First

---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncryptionApp
+{
+    public partial class Form1 : Form
+    {
+        private readonly string encryptionKey = "1234567890123456"; 
+        private readonly string encryptionIV = "abcdefghijklmnop"; 
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string plainText = textBox1.Text;
+
+            string encryptedText = Encrypt(plainText, encryptionKey, encryptionIV);
+
+            textBox2.Text = encryptedText;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private string Encrypt(string plainText, string key, string iv)
+        {
+            try
+            {
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Key = Encoding.UTF8.GetBytes(key);
+                    aes.IV = Encoding.UTF8.GetBytes(iv);
+
+                    ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                    using (var ms = new System.IO.MemoryStream())
+                    {
+                        using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                        {
+                            using (var sw = new System.IO.StreamWriter(cs))
+                            {
+                                sw.Write(plainText);
+                            }
+                        }
+                        return Convert.ToBase64String(ms.ToArray());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Encryption failed: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
